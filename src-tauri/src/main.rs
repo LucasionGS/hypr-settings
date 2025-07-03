4868ff898503852f5755ae26cec77092cff30153
LucasionArchion <lucasion@hotmail.com>
2025-07-03 22:09:54 +0200

    Update main function to set WebKit DMABUF renderer variable and adjust Tauri configuration for window properties

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // Set the environment variable to disable DMABUF renderer for WebKit
+    // This is needed for proper rendering in Hyprland and other compositors
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    
     hypr_settings_lib::run()
 }
